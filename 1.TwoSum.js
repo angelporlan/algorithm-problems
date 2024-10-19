15799b56d6e9fbcfd7698b7e51cfb75bee513739
angelporlan <angeelporlan4@gmail.com>
2024-10-19 17:23:53 +0200

    TwoSum solution

--- 1.TwoSum.js
+++ 1.TwoSum.js
@@ -1,6 +1,9 @@
 // Función que encuentra dos números en un array cuyo valor sumado es igual al objetivo (target).
 // Retorna un array con los índices de esos dos números, si existen, o null si no se encuentran.
 
+// Function that finds two numbers in an array whose sum value is equal to the target.
+// Returns an array with the indices of those two numbers, if they exist, or null if they are not found.
+
 const nums = [2,7,11,15]
 const target = 9
 
