7bd9a2218542c3fb30119d3e560e2c4efc6c361c
angelporlan <angeelporlan4@gmail.com>
2024-11-15 17:48:53 +0100

    5.SantaCybertruck added

---
+++ adventjs/5.SantaCibertruck.js
@@ -0,0 +1,22 @@
+function cyberReindeer(road, time) {
+    let moves = [road]
+    let a = 0
+    let b = "."
+  
+    for (let i = 1; i < time; i++) {
+      if (i == 5) road = road.replace(/\|/g, "*")
+      const newRoad = road.replace(/S[\.\*]/, `${b}S`)
+      if (newRoad != road) {
+        a++
+        b = road[a]
+      }
+      road = newRoad
+      moves.push(road)
+    }
+  
+    return moves;
+}
+
+let road = 'S..|...|..';
+const time = 10; 
+console.log(cyberReindeer(road, time));
