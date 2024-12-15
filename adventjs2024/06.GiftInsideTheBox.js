fa6286a85792cb8c783cb7687e652cd9f9b0d12b
angelporlan <angeelporlan4@gmail.com>
2024-12-15 12:42:16 +0100

    06.GiftInsideTheBox added

---
+++ adventjs2024/06.GiftInsideTheBox.js
@@ -0,0 +1,40 @@
+// Ya hemos empaquetado cientos de regalos 🎁… pero a un elfo se le ha olvidado revisar si el regalo, representado por un asterisco *, está dentro de la caja.
+
+// La caja tiene un regalo (*) y cuenta como dentro de la caja si:
+
+// Está rodeada por # en los bordes de la caja.
+// El * no está en los bordes de la caja.
+// Ten en cuenta entonces que el * puede estar dentro, fuera o incluso no estar. Y debemos devolver true si el * está dentro de la caja y false en caso contrario.
+
+function inBox(box) {
+    for (let i = 1; i < box.length - 1; i++) { 
+        const row = box[i];
+        
+        if (row.includes('*')) {
+            const giftIndex = row.indexOf('*'); 
+            if (giftIndex > 0 && giftIndex < row.length - 1) {
+                return true; 
+            } else {
+                return false; 
+            }
+        }
+    }
+    
+    return false;
+}
+
+console.log(inBox([
+    "#####",
+    "#   #",
+    "# * #",
+    "#   #",
+    "#####"
+])); // true
+
+console.log(inBox([
+    "#####",
+    "#   *",
+    "#   #",
+    "#   #",
+    "#####"
+])); // false
