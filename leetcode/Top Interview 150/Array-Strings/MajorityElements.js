0d8a73af21364b1187eef21b4d269783bd89ed77
angelporlan <angeelporlan4@gmail.com>
2026-01-15 20:14:28 +0100

    Complete MajorityElements exercise using Boyer-Moore algorithm

---
+++ leetcode/Top Interview 150/Array-Strings/MajorityElements.js
@@ -0,0 +1,43 @@
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,2,3]
+// Output: 3
+// Example 2:
+
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+ 
+
+// Constraints:
+
+// n == nums.length
+// 1 <= n <= 5 * 104
+// -109 <= nums[i] <= 109
+// The input is generated such that a majority element will exist in the array.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const majorityElement = function(nums) {
+    let candidate = null;
+    let count = 0;
+    
+    for (let num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+        count += (num === candidate) ? 1 : -1;
+    }
+    
+    return candidate;
+};
+
+console.log(majorityElement([3, 2, 3])); // Output: 3
+console.log(majorityElement([2, 2, 1, 1, 1, 2, 2])); // Output: 2
